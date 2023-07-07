d9dd59bdd2d8e7379324a1b0bb7a36b650effcf1
Tagellajore <tagelsft9@gmail.com>
2023-07-07 15:14:50 -0400

    making sql queries using javascript , node postgress

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+const config = {
+  user: 'labber',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+};
+
+const pool = new Pool(config);
+
+pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;`)
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`)
+    })
+  });
